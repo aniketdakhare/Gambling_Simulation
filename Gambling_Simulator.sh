a2e4eecc869c133cc3944261457b9e7060faaa33
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-08 13:21:30 +0530

    Created a Daily stake

--- Gambling_Simulator.sh
+++ Gambling_Simulator.sh
@@ -1,1 +1,8 @@
-echo "Welcome to Gambling Simulation Program "
+#!/bin/bash -x
+
+echo "Welcome to Gambling Simulator Program "
+
+Daily_Stake=100
+Bet=1
+
+echo "Daily_Stake is $ $Daily_Stake and Single Bet is of $ $Bet"
