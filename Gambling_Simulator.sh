b11d818fd34af7504c25c77c27dc88509b895f53
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-08 14:10:22 +0530

    Added limit to resign for a day

--- Gambling_Simulator.sh
+++ Gambling_Simulator.sh
@@ -1,24 +1,31 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "Welcome to Gambling Simulator Program "
 
 DAILY_STAKE=100
 BET=1
 WON=1
+percent=50
+limit=$(( $percent*$DAILY_STAKE/100 ))
+max_limit=$(( $DAILY_STAKE+$limit ))
+min_limit=$(( $DAILY_STAKE-$limit ))
+total_stake=$DAILY_STAKE
 
 function gambling()
 {
-	check=$((RANDOM%2))
-	if [ $check -eq $WON ]
-	then
-		DAILY_STAKE=$(( DAILY_STAKE+BET ))
-		echo "Won"
-        else
-                DAILY_STAKE=$(( DAILY_STAKE-BET ))
-                echo "Lost"
-        fi
+	while (( $total_stake > $min_limit && $total_stake < $max_limit ))
+	do
+		check=$((RANDOM%2))
+		if [ $check -eq $WON ]
+		then
+			total_stake=$((total_stake+BET))
+        	else
+                	total_stake=$((total_stake-BET))
+        	fi
+	done
+	echo "Done for the day..."
 }
 
 gambling
 
-echo "Total Stake is : $DAILY_STAKE"
+echo "Total Stake is : $total_stake"
